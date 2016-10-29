3a5d1c7282bf917567937aac495b7e6f22a24b6e
Igor Laborie <ilaborie@gmail.com>
2016-10-29 14:34:52 +0200

    with fallback

--- src/main/java/org/ilaborie/feign/client/Main.java
+++ src/main/java/org/ilaborie/feign/client/Main.java
@@ -5,23 +5,40 @@ import feign.gson.GsonEncoder;
 import feign.hystrix.HystrixFeign;
 import rx.Observable;
 
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
   public static void main(String[] args) {
     String url = "http://localhost:8080/api/cats";
+
+    // Fallback implementation
+    CatClient fallback = () -> {
+      Cat cat = new Cat("Chuck Norris's cat", Cat.CatRace.Sphynx);
+      return Observable.just(Collections.singletonList(cat));
+    };
 
     // Get Client with CircuitBreaker
     CatClient client = HystrixFeign.builder()
         .decoder(new GsonDecoder())
         .encoder(new GsonEncoder())
         .requestInterceptor(System.out::println)
-        .target(CatClient.class, url);
+        .target(CatClient.class, url, fallback);
 
-    // Find All
-    client.findAll()
-        .flatMap(Observable::from)
-        .map(Cat::getName)
-        .toBlocking()
+    // Find All every 2 seconds
+    Observable.interval(2, TimeUnit.SECONDS)
+        .flatMap(i -> client.findAll())
         .forEach(System.out::println);
+
+    waiting();
+  }
+
+  private static void waiting() {
+    try {
+      Thread.sleep(TimeUnit.MINUTES.toMillis(5));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
