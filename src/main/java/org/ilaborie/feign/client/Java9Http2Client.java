9d1d0c9492a95aa8e56e09bf744d5c7245abecf7
Igor Laborie <ilaborie@gmail.com>
2016-10-31 15:17:20 +0100

    java9

--- src/main/java/org/ilaborie/feign/client/Java9Http2Client.java
+++ src/main/java/org/ilaborie/feign/client/Java9Http2Client.java
@@ -5,35 +5,58 @@ import feign.Request;
 import feign.Response;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 public class Java9Http2Client implements Client {
+
     @Override
     public Response execute(Request request, Request.Options options) throws IOException {
         try {
             System.out.println("Using JEP 110");
-
+            HttpRequest.Builder builder = HttpRequest.create(new URI(request.url()));
             // handle body
-            byte[] reqBody = null;
+            byte[] reqBody = request.body();
+            if (reqBody == null || reqBody.length == 0) {
+                builder = builder.body(HttpRequest.noBody());
+            } else {
+                builder = builder.body(HttpRequest.fromByteArray(reqBody));
+            }
 
             // apply headers
+            for (Map.Entry<String, Collection<String>> entry : request.headers().entrySet()) {
+                String values = entry.getValue().stream()
+                        .collect(Collectors.joining(", "));
+                builder = builder.setHeader(entry.getKey(), values);
+            }
 
             // Execute request
+            HttpResponse response = builder.method(request.method())
+                    .response();
 
             // retrieve values
-            int status = 000;
-            byte[] body = null;
-            Map<String, Collection<String>> headers = null;
+            int status = response.statusCode();
+            byte[] body = response.body(HttpResponse.asByteArray());
+            Map<String, Collection<String>> headers = mapList2MapCollection(
+                    response.headers().map()
+            );
 
             // Build Feign response
-            return null;
-        } finally {
-            System.out.println("Done");
+            return Response.builder()
+                    .status(status)
+                    .headers(headers)
+                    .body(body)
+                    .build();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
         }
     }
 
--- src/main/java/org/ilaborie/feign/client/Main.java
+++ src/main/java/org/ilaborie/feign/client/Main.java
@@ -8,39 +8,40 @@ import java.util.List;
 
 public class Main {
 
-  public static void main(String[] args) {
-    // Display Java Version (New Java 9 Feature)
-    System.out.println("Java version: " + Runtime.version());
+    public static void main(String[] args) {
+        // Display Java Version (New Java 9 Feature)
+        System.out.println("Java version: " + Runtime.version());
 
-    String url = "http://localhost:8080/api/cats";
-    CatClient client = Feign.builder()
-        .decoder(new GsonDecoder())
-        .encoder(new GsonEncoder())
-        .requestInterceptor(System.out::println)
-        // TODO Java9 Http Client
-        .target(CatClient.class, url);
+        String url = "http://localhost:8080/api/cats";
+        CatClient client = Feign.builder()
+                .decoder(new GsonDecoder())
+                .encoder(new GsonEncoder())
+                .requestInterceptor(System.out::println)
+                // Java9 Http Client
+                .client(new Java9Http2Client())
+                .target(CatClient.class, url);
 
-    // Find All
-    List<Cat> cats = client.findAll();
-    cats.forEach(System.out::println);
-    System.out.println();
+        // Find All
+        List<Cat> cats = client.findAll();
+        cats.forEach(System.out::println);
+        System.out.println();
 
-    // Find by id
-    String id = cats.stream().map(Cat::getId).findFirst().orElse(null);
-    Cat c = client.findById(id);
-    System.out.println("Cat: " + c);
-    System.out.println();
+        // Find by id
+        String id = cats.stream().map(Cat::getId).findFirst().orElse(null);
+        Cat c = client.findById(id);
+        System.out.println("Cat: " + c);
+        System.out.println();
 
-    // Create
-    Cat newCat = new Cat("Felix", Cat.CatRace.Angora);
-    Cat created = client.create(newCat);
-    System.out.println("New Cat: " + created);
-    System.out.println();
+        // Create
+        Cat newCat = new Cat("Felix", Cat.CatRace.Angora);
+        Cat created = client.create(newCat);
+        System.out.println("New Cat: " + created);
+        System.out.println();
 
-    // Delete
-    client.delete(created.getId());
-    System.out.println("cat deleted");
-    System.out.println();
-  }
+        // Delete
+        client.delete(created.getId());
+        System.out.println("cat deleted");
+        System.out.println();
+    }
 
 }
