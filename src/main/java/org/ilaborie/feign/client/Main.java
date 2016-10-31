610ec6e019fe835a73d6b0c90d8d4e11bf9855bd
Igor Laborie <ilaborie@gmail.com>
2016-10-31 23:06:56 +0100

    Base

---
+++ src/main/java/org/ilaborie/feign/client/Main.java
@@ -0,0 +1,29 @@
+package org.ilaborie.feign.client;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String url = "http://localhost:8080/api/cats";
+
+    // Get Client
+
+    // Find All
+    System.out.println();
+
+    // Find by id
+    String id = null;
+    System.out.println();
+
+    // Create
+    Cat newCat = null;
+    System.out.println();
+
+    // Delete
+    System.out.println();
+
+    // Add custom header
+    // Tips debug lite
+    // Tips debug logger
+  }
+
+}
